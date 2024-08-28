2441b21e98355003c092057a69953356e77c494e
Ronaldo Hauser Dias <57193790+ronaldohauser@users.noreply.github.com>
2024-08-28 15:54:36 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('.nav');
+
+    menuToggle.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
